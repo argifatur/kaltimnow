b52cdfde7d23274137ca61b101e94338546864d9
Refi Fahreza <zylgwn211@gmail.com>
2019-03-28 16:41:34 +0800

    web
    yang kurang

--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,11 @@ Auth::routes();
 Route::get('/home',  'HomeController@index')->name('index');
 Route::get('/',      'MainController@index')->name('/');
 Route::get('/berita/{path}',  'MainController@showBerita')->name('berita');
-Route::get('/kategori/{cat}', 'MainController@kategoriBerita')->name('kategori');
 Route::post('/cari',		  'MainController@cari')->name('cari');
+Route::post('/categori',		  'MainController@categori')->name('categori');
+Route::get('/post',		  'MainController@post')->name('post');
+Route::get('/about',		  'MainController@about')->name('about');
+Route::get('/daftarpost',		  'MainController@daftarpost')->name('daftarpost');
 
 /* -------------- Admin -----------*/
 
